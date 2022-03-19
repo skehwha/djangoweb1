93b67a9710cdd9c3cad0da47101a88cf5012de36
skehwha <wsym1714@naver.com>
2022-03-19 17:50:15 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.shortcuts import render
 
 class PostList(ListView):
@@ -37,6 +37,21 @@ def category_page(request, slug):
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
+        }
+    )
+
+def tag_page(request, slug):
+    tag=Tag.objects.get(slug=slug)
+    post_list=tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'tag': tag,
         }
     )
 
