cdadd57a547a784fcf02f92fec76f368b992c179
skehwha <wsym1714@naver.com>
2022-03-20 17:47:42 +0900

    로그인 사용자에게만 포스트 작성 허용

--- blog/tests.py
+++ blog/tests.py
@@ -168,3 +168,30 @@ class TestView(TestCase):
         self.assertIn(self.post_001.title, main_area.text)
         self.assertNotIn(self.post_002.title, main_area.text)
         self.assertNotIn(self.post_003.title, main_area.text)
+
+    def test_create_post(self):
+        #비로그인시 status code != 200
+        response=self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
+
+        #로그인시
+        self.client.login(username='trump', password='somepassword')
+
+        response=self.client.get('/blog/create_post/')
+        self.assertEqual(response.status_code, 200)
+        soup=BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Create Post - Blog', soup.title.text)
+        main_area=soup.find('div', id='main-area')
+        self.assertIn('Create New Post', main_area.text)
+
+        self.client.post(
+            '/blog/create_post/',
+            {
+                'title': 'Post Form 만들기',
+                'content': 'Post Form 만들기',
+            }
+        )
+        last_post=Post.objects.last()
+        self.assertEqual(last_post.title, 'Post Form 만들기')
+        self.assertEqual(last_post.author.username, 'trump')
